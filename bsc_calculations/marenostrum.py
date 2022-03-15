37ad2ae79bb9772d573283afa029cf7726d8e19d
Martin-Floor <martin.floor@uvic.cat>
2022-03-15 17:15:05 +0100

    added marenostrum

--- bsc_calculations/marenostrum.py
+++ bsc_calculations/marenostrum.py
@@ -1,6 +1,6 @@
 def jobArrays(jobs, script_name=None, job_name=None, cpus=1, mem_per_cpu=None,
               partition=None, threads=None, output=None, mail=None, time=48,
-              modules=None, conda_env=None, unload_modules=None):
+              modules=None, conda_env=None, unload_modules=None, program=None, conda_eval_bash=False):
     """
     Set up job array scripts for marenostrum slurm job manager.
 
@@ -11,6 +11,21 @@ def jobArrays(jobs, script_name=None, job_name=None, cpus=1, mem_per_cpu=None,
     script_name : str
         Name of the SLURM submission script.
     """
+
+    available_programs = ['pele', 'peleffy']
+    if program != None:
+        if program not in available_programs:
+            raise ValueError('Program not found. Available progams: '+' ,'.join(available_programs))
+
+    if program == 'pele' or program == 'peleffy':
+        if modules == None:
+            modules = []
+        modules += modules+['ANACONDA/2019.10', 'intel', 'mkl', 'impi', 'gcc', 'boost/1.64.0']
+        conda_eval_bash = True
+        if program == 'pele':
+            conda_env = '/gpfs/projects/bsc72/conda_envs/platform/1.6.3'
+        elif program == 'peleffy':
+            conda_env = '/gpfs/projects/bsc72/conda_envs/peleffy/1.3.4'
 
     available_partitions = ['debug', 'bsc_ls']
     if job_name == None:
@@ -47,9 +62,9 @@ def jobArrays(jobs, script_name=None, job_name=None, cpus=1, mem_per_cpu=None,
     with open(script_name,'w') as sf:
         sf.write('#!/bin/bash\n')
         sf.write('#SBATCH --job-name='+job_name+'\n')
-        sf.write('#SBATCH --partition='+partition+'\n')
+        sf.write('#SBATCH --qos='+partition+'\n')
         sf.write('#SBATCH --time='+str(time)+':00:00\n')
-        sf.write('#SBATCH -n '+str(cpus)+'\n')
+        sf.write('#SBATCH --ntasks '+str(cpus)+'\n')
         if mem_per_cpu != None:
             sf.write('#SBATCH --mem-per-cpu '+str(mem_per_cpu)+'\n')
         if threads != None:
@@ -70,6 +85,8 @@ def jobArrays(jobs, script_name=None, job_name=None, cpus=1, mem_per_cpu=None,
             for module in modules:
                 sf.write('module load '+module+'\n')
             sf.write('\n')
+        if conda_eval_bash:
+            sf.write('eval "$(conda shell.bash hook)"\n')
         if conda_env != None:
             sf.write('source activate '+conda_env+'\n')
             sf.write('\n')
@@ -82,6 +99,100 @@ def jobArrays(jobs, script_name=None, job_name=None, cpus=1, mem_per_cpu=None,
                 sf.write('fi\n')
             else:
                 sf.write('\nfi\n')
+            sf.write('\n')
+
+    if conda_env != None:
+        with open(script_name,'a') as sf:
+            sf.write('conda deactivate \n')
+            sf.write('\n')
+
+def singleJob(job, script_name=None, job_name=None, cpus=96, mem_per_cpu=None,
+              partition=None, threads=None, output=None, mail=None, time=48,
+              modules=None, conda_env=None, unload_modules=None, program=None, conda_eval_bash=False):
+
+    available_programs = ['pele']
+    if program != None:
+        if program not in available_programs:
+            raise ValueError('Program not found. Available progams: '+' ,'.join(available_programs))
+
+    if program == 'pele':
+        if modules == None:
+            modules = []
+        modules += modules+['ANACONDA/2019.10', 'intel', 'mkl', 'impi', 'gcc', 'boost/1.64.0']
+        conda_eval_bash = True
+        conda_env = '/gpfs/projects/bsc72/conda_envs/platform/1.6.3'
+
+    available_partitions = ['debug', 'bsc_ls']
+    if job_name == None:
+        raise ValueError('job_name == None. You need to specify a name for the job')
+    if output == None:
+        output = job_name
+    if partition == None:
+        raise ValueError('You must select a partion. Available partitions are:'+
+                         ', '.join(available_partitions))
+    if partition not in available_partitions:
+        raise ValueError('Wrong partition selected. Available partitions are:'+
+                         ', '.join(available_partitions))
+    if script_name == None:
+        script_name = 'slurm_array.sh'
+    if modules != None:
+        if isinstance(modules, str):
+            modules = [modules]
+        if not isinstance(modules, list):
+            raise ValueError('Modules to load must be given as a list or as a string (for loading one module only)')
+    if unload_modules != None:
+        if isinstance(unload_modules, str):
+            unload_modules = [unload_modules]
+        if not isinstance(unload_modules, list):
+            raise ValueError('Modules to unload must be given as a list or as a string (for unloading one module only)')
+    if conda_env != None:
+        if not isinstance(conda_env, str):
+            raise ValueError('The conda environment must be given as a string')
+
+    if partition == 'debug':
+        time = 2
+    elif partition == 'bsc_ls':
+        if time > 48:
+            print('Setting time at maximum allowed for the bsc_ls partition (48 hours).')
+            time=48
+
+    #Write jobs as array
+    with open(script_name,'w') as sf:
+        sf.write('#!/bin/bash\n')
+        sf.write('#SBATCH --job-name='+job_name+'\n')
+        sf.write('#SBATCH --qos='+partition+'\n')
+        sf.write('#SBATCH --time='+str(time)+':00:00\n')
+        sf.write('#SBATCH --ntasks '+str(cpus)+'\n')
+        if mem_per_cpu != None:
+            sf.write('#SBATCH --mem-per-cpu '+str(mem_per_cpu)+'\n')
+        if threads != None:
+            sf.write('#SBATCH -c '+str(threads)+'\n')
+        sf.write('#SBATCH --output='+output+'_%a_%A.out\n')
+        sf.write('#SBATCH --error='+output+'_%a_%A.err\n')
+        if mail != None:
+            sf.write('#SBATCH --mail-user='+mail+'\n')
+            sf.write('#SBATCH --mail-type=END,FAIL\n')
+        sf.write('\n')
+
+        if unload_modules != None:
+            for module in unload_modules:
+                sf.write('module unload '+module+'\n')
+            sf.write('\n')
+        if modules != None:
+            for module in modules:
+                sf.write('module load '+module+'\n')
+            sf.write('\n')
+        if conda_eval_bash:
+            sf.write('eval "$(conda shell.bash hook)"\n')
+        if conda_env != None:
+            sf.write('source activate '+conda_env+'\n')
+            sf.write('\n')
+
+    with open(script_name,'a') as sf:
+        sf.write(job)
+        if not job.endswith('\n'):
+            sf.write('\n\n')
+        else:
             sf.write('\n')
 
     if conda_env != None:
