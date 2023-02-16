075f475c01ade2df607f0af801517c376366cb0c
Martin-Floor <martinfloor@gmail.com>
2023-02-16 12:32:04 +0100

    Fixed typo on group_jobs_by

--- bsc_calculations/marenostrum.py
+++ bsc_calculations/marenostrum.py
@@ -35,7 +35,7 @@ def jobArrays(jobs, script_name=None, job_name=None, cpus=1, mem_per_cpu=None, h
         gj = ''
         for i,j in enumerate(jobs):
             gj += j
-            if  (i+1) % groups_jobs_by == 0:
+            if  (i+1) % group_jobs_by == 0:
                 grouped_jobs.append(gj)
                 gj = ''
         if gj != '':
