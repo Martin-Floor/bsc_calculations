83896867265ea950891a59cc94283ccfe384bc65
ruite <ruite.xiang@gmail.com>
2023-08-04 12:45:44 +0200

    revert to 2 GPUs for AF2

--- bsc_calculations/cte_power.py
+++ bsc_calculations/cte_power.py
@@ -47,9 +47,9 @@ def jobArrays(jobs, script_name=None, job_name=None, cpus_per_task=40, gpus=1, n
             modules = ['singularity', 'alphafold/2.1.0_tf2.6.0']
         else:
             modules += ['singularity', 'alphafold/2.1.0_tf2.6.0']
-        cpus_per_task = 40
+        cpus_per_task = 80
         ntasks = 1
-        gpus = 1
+        gpus = 2
         if pythonpath == None:
             pythonpath = ['/opt/conda/lib/python3.7/site-packages']
         else:
