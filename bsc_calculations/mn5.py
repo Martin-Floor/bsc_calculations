2eac6a6beb00fe4881beadc965ade34ca805ec10
mluengo <miguelluengo.bcn@gmail.com>
2024-05-13 13:03:42 +0200

    Added jobarrays to mn5 for gromacs

--- bsc_calculations/mn5.py
+++ bsc_calculations/mn5.py
@@ -1,6 +1,188 @@
 import os
 
 
+def jobArrays(jobs, script_name=None, job_name=None, ntasks=1, gpus=1 ,mem_per_cpu=None, highmem=False,
+              partition='bsc_ls', cpus_per_task=None, output=None, mail=None, time=48, module_purge=False,
+              modules=None, conda_env=None, unload_modules=None, program=None, conda_eval_bash=False, account='bsc72',
+              jobs_range=None, group_jobs_by=None, pythonpath=None, local_libraries=False, msd_version=None, mpi=False,
+              pathMN=None):
+    """
+    Set up job array scripts for marenostrum slurm job manager.
+
+    Parameters
+    ==========
+    jobs : list
+        List of jobs. Each job is a string representing the command to execute.
+    script_name : str
+        Name of the SLURM submission script.
+    jobs_range : (list, tuple)
+        The range of job IDs to be included in the job array (one-based numbering).
+        This restart the indexing of the slurm array IDs, so keep count of the original
+        job IDs based on the supplied jobs list. Also, the range includes the last job ID.
+        Useful when large IDs cannot enter the queue.
+    group_jobs_by : int
+        Group jobs to enter in the same job array (useful for launching many short
+        jobs when there are a max_job_allowed limit per user.
+    local_libraries : bool
+        Add local libraries (e.g., prepare_proteins) to PYTHONPATH?
+    """
+
+    # Check input
+    if isinstance(jobs, str):
+        jobs = [jobs]
+
+    if jobs_range != None:
+        if not isinstance(jobs_range, (list, tuple)) or len(jobs_range) != 2 or not all([isinstance(x, int) for x in jobs_range]):
+            raise ValueError('The given jobs_range must be a tuple or a list of 2-integers')
+
+    # Group jobs to enter in the same job array (useful for launching many short
+    # jobs when there are a max_job_allowed limit per user.)
+    if isinstance(group_jobs_by, int):
+        grouped_jobs = []
+        gj = ''
+        for i,j in enumerate(jobs):
+            gj += j
+            if  (i+1) % group_jobs_by == 0:
+                grouped_jobs.append(gj)
+                gj = ''
+        if gj != '':
+            grouped_jobs.append(gj)
+        jobs = grouped_jobs
+
+    elif not isinstance(group_jobs_by, type(None)):
+        raise ValueError('You must give an integer to group jobs by this number.')
+
+    # Check PYTHONPATH variable
+    if pythonpath == None:
+        pythonpath = []
+
+    if pathMN == None:
+        pathMN = []
+
+    available_programs = ['gromacs']
+
+    #available_programs = ['pele', 'peleffy', 'rosetta', 'predig', 'pyrosetta', 'rosetta2', 'blast',
+    #                      'msd', 'pml', 'netsolp', 'alphafold', 'asitedesign']
+
+    if program != None:
+        if program not in available_programs:
+            raise ValueError('Program not found. Available progams: '+' ,'.join(available_programs))
+
+    if program == 'gromacs':
+      if modules == None:
+          modules = ['cuda','nvidia-hpc-sdk/23.11','gromacs/2023.3']
+      else:
+          modules += ['cuda','nvidia-hpc-sdk/23.11','gromacs/2023.3']
+
+    available_partitions = ['acc_debug', 'acc_bscls']
+
+    if job_name == None:
+        raise ValueError('job_name == None. You need to specify a name for the job')
+    if output == None:
+        output = job_name
+    if partition not in available_partitions:
+        raise ValueError('Wrong partition selected. Available partitions are:'+
+                         ', '.join(available_partitions))
+
+    if script_name == None:
+        script_name = 'slurm_array.sh'
+    elif not script_name.endswith('.sh'):
+        script_name += '.sh'
+
+    if modules != None:
+        if isinstance(modules, str):
+            modules = [modules]
+        if not isinstance(modules, list):
+            raise ValueError('Modules to load must be given as a list or as a string (for loading one module only)')
+    if unload_modules != None:
+        if isinstance(unload_modules, str):
+            unload_modules = [unload_modules]
+        if not isinstance(unload_modules, list):
+            raise ValueError('Modules to unload must be given as a list or as a string (for unloading one module only)')
+    if conda_env != None:
+        if not isinstance(conda_env, str):
+            raise ValueError('The conda environment must be given as a string')
+
+    if partition == 'debug':
+        time = 2
+    elif partition == 'bsc_ls':
+        if time > 48:
+            print('Setting time at maximum allowed for the bsc_ls partition (48 hours).')
+            time=48
+
+    # Slice jobs if a range is given
+    if jobs_range != None:
+        jobs = jobs[jobs_range[0]-1:jobs_range[1]]
+
+    #Write jobs as array
+    with open(script_name,'w') as sf:
+        sf.write('#!/bin/bash\n')
+        sf.write('#SBATCH --job-name='+job_name+'\n')
+        sf.write('#SBATCH --qos='+partition+'\n')
+        sf.write('#SBATCH --time='+str(time)+':00:00\n')
+        sf.write('#SBATCH --ntasks '+str(ntasks)+'\n')
+        sf.write('#SBATCH --gres gpu:'+str(gpus)+'\n')
+        sf.write('#SBATCH --account='+account+'\n')
+        if highmem:
+            sf.write('#SBATCH --constraint=highmem\n')
+        if mem_per_cpu != None:
+            sf.write('#SBATCH --mem-per-cpu '+str(mem_per_cpu)+'\n')
+        if cpus_per_task != None:
+            sf.write('#SBATCH --cpus-per-task '+str(cpus_per_task)+'\n')
+        sf.write('#SBATCH --array=1-'+str(len(jobs))+'\n')
+        sf.write('#SBATCH --output='+output+'_%a_%A.out\n')
+        sf.write('#SBATCH --error='+output+'_%a_%A.err\n')
+        if mail != None:
+            sf.write('#SBATCH --mail-user='+mail+'\n')
+            sf.write('#SBATCH --mail-type=END,FAIL\n')
+        sf.write('\n')
+
+        if module_purge:
+                sf.write('module purge\n')
+        if unload_modules != None:
+            for module in unload_modules:
+                sf.write('module unload '+module+'\n')
+            sf.write('\n')
+        if modules != None:
+            for module in modules:
+                sf.write('module load '+module+'\n')
+            sf.write('\n')
+        if conda_eval_bash:
+            sf.write('eval "$(conda shell.bash hook)"\n')
+        if conda_env != None:
+            sf.write('source activate '+conda_env+'\n')
+            sf.write('\n')
+
+        for pp in pythonpath:
+            sf.write('export PYTHONPATH=$PYTHONPATH:'+pp+'\n')
+            sf.write('\n')
+
+        for pp in pathMN:
+            sf.write('export PATH=$PATH:'+pp+'\n')
+            sf.write('\n')
+
+        if program == 'gromacs':
+            sf.write('export SLURM_CPU_BIND=none\n')
+            sf.write('export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK\n')
+            sf.write('export GMX_ENABLE_DIRECT_GPU_COMM=1\n')
+            sf.write('export GMX_GPU_PME_DECOMPOSITION=1\n')
+            sf.write('GMXBIN="mpirun --bind-to none -report-bindings gmx_mpi"\n')
+
+    for i in range(len(jobs)):
+        with open(script_name,'a') as sf:
+            sf.write('if [[ $SLURM_ARRAY_TASK_ID = '+str(i+1)+' ]]; then\n')
+            sf.write(jobs[i])
+            if jobs[i].endswith('\n'):
+                sf.write('fi\n')
+            else:
+                sf.write('\nfi\n')
+            sf.write('\n')
+
+    if conda_env != None:
+        with open(script_name,'a') as sf:
+            sf.write('conda deactivate \n')
+            sf.write('\n')
+
 def setUpPELEForMarenostrum(jobs, general_script='pele_slurm.sh', scripts_folder='pele_slurm_scripts',
                             print_name=False, **kwargs):
     """
@@ -25,11 +207,11 @@ def setUpPELEForMarenostrum(jobs, general_script='pele_slurm.sh', scripts_folder
             singleJob(job, job_name=job_name, script_name=scripts_folder+'/'+job_name+'.sh', program='pele', **kwargs)
             if print_name:
                 ps.write('echo Launching job '+job_name+'\n')
-            ps.write('sbatch -A bsc72 '+scripts_folder+'/'+job_name+'.sh\n')
+            ps.write('sbatch '+scripts_folder+'/'+job_name+'.sh\n')
 
 
 def singleJob(job, script_name=None, job_name=None, cpus=112, mem_per_cpu=None, highmem=False,
-              partition=None, threads=None, output=None, mail=None, time=None, pythonpath=None,
+              partition=None, threads=None, output=None, mail=None, time=None, pythonpath=None, account='bsc72',
               modules=None, conda_env=None, unload_modules=None, program=None, conda_eval_bash=False, pathMN=None):
 
     # Check PYTHONPATH variable
@@ -47,7 +229,7 @@ def singleJob(job, script_name=None, job_name=None, cpus=112, mem_per_cpu=None, 
     if program == 'pele':
         if modules == None:
             modules = []
-        modules += modules+['anaconda', 'intel', 'impi', 'mkl', 'cmake', 'transfer', 'bsc']
+        modules += modules+['intel', 'impi', 'mkl', 'cmake', 'boost' ,'anaconda', 'bsc', 'transfer']
         conda_eval_bash = True
         conda_env = '/gpfs/projects/bsc72/conda_envs/platform'
 
@@ -101,6 +283,7 @@ def singleJob(job, script_name=None, job_name=None, cpus=112, mem_per_cpu=None, 
         sf.write('#SBATCH --qos='+partition+'\n')
         sf.write('#SBATCH --time='+str(time[0])+':'+str(time[1])+':00\n')
         sf.write('#SBATCH --ntasks '+str(cpus)+'\n')
+        sf.write('#SBATCH --account='+account)
 
         # Have to check if these work
         # ---
